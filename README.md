98be5eb2d791469f97d66c6fecc9431e19ddddb8
Андрей Владимирович Комаров <komarov@avt.local>
2022-10-27 09:44:13 +0300

    feat: Add package source

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
-# gd32f10x-pack-repo
+# GigaDevice GD32F10x CMSIS Pack Repository
+
+This repository contains an unofficial GigaDevice GD32F10x CMSIS Pack for [GNU MCU Eclipse][packs-manager].
+
+Based on [Milandr MCU 1986x CMSIS Pack Repository][https://github.com/in4lio/mdr1986x-pack-repo]
+
+### How to install the package (Eclipse)
+
+- [Add][packs-manager-config] Milandr MCU repository, specifying the path to [_"index.pidx"_][index.pidx]
+file:
+```
+Eclipse Menu → Window → Preferences → C/C++ → Packages → Repositories → Add...
+
+Type       CMSIS Pack
+Name       GigaDevice GD32F10x
+URL        https://github.com/kamarado/gd32f10x-pack-repo/raw/master/index.pidx
+```
+With old versions of Packs Manager, you should use [_"index.idx"_][index.idx] file.
+
+- [Open][packs-manager-persp] the Packs perspective and [install][packs-manager-install] `GD32F10x`
+package from `GigaDevice` group.
+
+- The path to the packages is defined in `packs_path` macro, but for the plug-in version that I use,
+this macro is not visible from tools configurations. Therefore, you may also need to set `packs_path`
+macro manually:
+```
+Eclipse Menu → Window → Preferences → Run/Debug → String Substitution → New...
+
+Name       packs_path
+Value      <path to packages>
+```
+
+### How to examine/modify the peripheral registers (Eclipse)
+
+[The peripherals registers view][debug-registers]
+
+### How to rebuild the package
+
+- Clone this repository and its submodules if they are not cloned yet:
+
+```
+git clone https://github.com/kamarado/gd32f01x-pack-repo.git
+cd gd32f10x-pack-repo
+git submodule update --init --recursive
+```
+
+- Or, update this repository and submodules if they are already cloned:
+
+```
+cd gd32f10x-pack-repo
+git pull origin master
+git submodule foreach git pull origin master
+```
+
+- Run the build script with Python 3:
+
+```
+python build.py
+```
+
+[packinstaller]:         http://www2.keil.com/mdk5/packinstaller/
+[packs-manager]:         https://gnu-mcu-eclipse.github.io/plugins/packs-manager/
+[packs-manager-config]:  https://gnu-mcu-eclipse.github.io/plugins/packs-manager/#configuration
+[packs-manager-persp]:   https://gnu-mcu-eclipse.github.io/plugins/packs-manager/#the-packs-perspective
+[packs-manager-install]: https://gnu-mcu-eclipse.github.io/plugins/packs-manager/#pack-install
+[debug-registers]:       https://gnu-mcu-eclipse.github.io/debug/peripheral-registers/
+[pack]:                  https://raw.githubusercontent.com/kamarado/gd32f10x-pack-repo/master/GigaDevice.GD32F10x_DFP.2.0.2.pack
+[index.pidx]:            https://raw.githubusercontent.com/kamarado/gd32f10x-pack-repo/master/index.pidx
+[index.idx]:             https://raw.githubusercontent.com/kamarado/gd32f10x-pack-repo/master/index.idx
---
+++ build.py
@@ -0,0 +1,111 @@
+r"""
+build.py -- Build GigaDevice GD32F10x CMSIS Package
+"""
+
+import os
+import glob
+import shutil
+import time
+from distutils.dir_util import copy_tree
+from distutils.dir_util import remove_tree
+from distutils.file_util import copy_file
+from distutils.file_util import move_file
+from xml.dom import minidom
+
+SOURCE_DIR = './source/'
+BUILD_DIR  = './build/'
+PDSC_FILE  = './GigaDevice.GD32F10x.pdsc'
+PACK_TEMPL = './GigaDevice.GD32F10x.%s.pack'
+IDX_FILE   = './index.pidx'
+
+copylist = (
+    ( 'CMSIS/', 'Libraries/CMSIS/' ),
+    ( 'GD32F10x_standard_peripheral/', 'Libraries/GD32F10x_standard_peripheral/' ),
+    ( 'GD32F10x_usbd_library/', 'Libraries/GD32F10x_usbd_library/' ),
+    ( 'GD32F10x_usbfs_library/', 'Libraries/GD32F10x_usbfs_library/' ),
+
+    ( 'Example_Projects/', 'Example_Projects/' ),
+)
+
+#  @ - remove according to the list of files
+dellist = (
+    '@Example_Projects/.gitignore',
+)
+
+print('Clean build')
+if os.path.exists( BUILD_DIR ):
+    remove_tree( BUILD_DIR )
+    time.sleep( 0.5 )
+
+os.makedirs( BUILD_DIR )
+
+ver = minidom.parse( PDSC_FILE ).getElementsByTagName( 'release' )[ 0 ].attributes[ 'version' ].value
+print('Version', ver)
+
+pack = PACK_TEMPL % ver
+
+if os.path.exists( pack ):
+    print('Backup', pack)
+    bak = pack + '.bak'
+    if os.path.exists( bak ):
+        os.remove( bak )
+    move_file( pack, bak )
+
+print('Copy')
+print(' ', PDSC_FILE)
+copy_file( PDSC_FILE, BUILD_DIR )
+
+for op in copylist:
+    p = os.path.join( BUILD_DIR, op[ 1 ])
+    if not os.path.exists( p ):
+        os.makedirs( p )
+    print('Copy to', p)
+    n = 0
+    for f in glob.glob( os.path.join( SOURCE_DIR, op[ 0 ])):
+        print(' ', f)
+        n += 1
+        if os.path.isdir( f ):
+            copy_tree( f, p )
+        else:
+            copy_file( f, p )
+
+    if n == 0:
+        print('*** ERROR *** Nothing was copied here.')
+        print()
+
+print('Remove')
+for op in dellist:
+    if op.startswith( '@' ):
+        f = os.path.join( BUILD_DIR, op[ 1: ])
+        d = os.path.dirname( f )
+        try:
+            sublist = open( f ).read().splitlines()
+            sublist = [ os.path.join( d, x[ 1: ]) if x.startswith( '/' ) else os.path.join( d, x ) for x in sublist if x.strip()]
+        except IOError:
+            sublist = []
+            print('*** ERROR *** Failed to get a removal list from "%s".' % ( f ))
+            print()
+    else:
+        sublist = [ os.path.join( BUILD_DIR, op )]
+    for f in sublist:
+        print(' ', f)
+        if os.path.isdir( f ):
+            remove_tree( f )
+        else:
+            try:
+                os.remove( f )
+            except OSError:
+                pass
+
+print('Zip to', pack)
+shutil.make_archive( pack, format='zip', root_dir=BUILD_DIR )
+move_file( pack + '.zip', pack )
+
+print('Write version to', IDX_FILE)
+idx = minidom.parse( IDX_FILE )
+idx.getElementsByTagName( 'pdsc' )[ 0 ].attributes[ 'version' ].value = ver
+print(idx.toxml())
+idx.writexml( open( IDX_FILE, "wb" ))
+
+open( os.path.join( BUILD_DIR, '.gitignore' ), 'w' ).write( '*' )
+print('Done')
